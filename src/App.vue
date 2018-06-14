21f3fa36a587ac8dcca407e5460ec3941f2381db
石一龙 <wb_shiyilong@opg.cn>
2018-06-14 14:46:22 +0800

    update demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+html, body {
+  height: 100vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #f9f9f9;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/example.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    hello word
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.hello{
+  width: 100%;
+  height: 100%;
+  padding-top: 50px;
+}
+</style>
