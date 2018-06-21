686f0a91cba3f7e1b547f18ff9e785f9fd921bab
石一龙 <wb_shiyilong@opg.cn>
2018-06-21 10:23:51 +0800

    更新路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,33 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Example from '@/components/example'
 
-//    路由的集合
-const RouterList = [Example]
-
-let ChildrenList = RouterList.map(item => {
-  return {
-    path: `/${item.name}`,
-    name: `${item.name}`,
-    component: item
-  }
-})
+// // 路由的集合
+// const RouterList = [Example]
+//
+// // 根据页面去渲染路由
+// let ChildrenList = RouterList.map(item => {
+//   return {
+//     path: `/${item.name}`,
+//     name: `${item.name}`,
+//     component: item
+//   }
+// })
+//
+// Vue.use(Router)
+//
+// export default new Router({
+//   routes: ChildrenList
+// })
 
 Vue.use(Router)
 
+// 一般的路由配置
 export default new Router({
-  routes: ChildrenList
+  routes: [
+    {
+      path: '/',
+      name: 'Example',
+      component: Example
+    }
+  ]
 })
